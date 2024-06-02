2cdcf7c784ec884d37552316fffe193cab7f88b0
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-06-02 14:02:36 -0400

    style: change style cards messages, flex items

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="card" v-for="message in mappedMessages" :key="message.id">
-    <div class="content">
-      <svg
-        fill="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M20 9V5H4V9H20ZM20 11H4V19H20V11ZM3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM5 12H8V17H5V12ZM5 6H7V8H5V6ZM9 6H11V8H9V6Z"
-        ></path>
-      </svg>
-      <section>
-        <p class="para">{{ message.email }}</p>
-        <p class="para">{{ message.message }}</p>
-        <p class="para">{{ message.createdAt }}</p>
-      </section>
+  <div class="card-container">
+    <div class="card" v-for="message in mappedMessages" :key="message.id">
+      <div class="content">
+        <svg
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M20 9V5H4V9H20ZM20 11H4V19H20V11ZM3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM5 12H8V17H5V12ZM5 6H7V8H5V6ZM9 6H11V8H9V6Z"
+          ></path>
+        </svg>
+        <section>
+          <p class="para">{{ message.email }}</p>
+          <p class="para">{{ message.message }}</p>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -66,6 +67,14 @@ export default {
 </script>
 
 <style scoped>
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 100px;
+  justify-content: center;
+  padding: 50px;
+}
+
 .card {
   position: relative;
   display: flex;
@@ -86,7 +95,7 @@ export default {
   border-radius: 22px;
   color: #ffffff;
   overflow: hidden;
-  background: #0a3cff;
+  background: #42b983;
   transition: all 0.48s cubic-bezier(0.23, 1, 0.32, 1);
 }
 
